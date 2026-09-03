ee332f9db0882274af56b4097d4b2ed0b63b4cc3
k632412550051-ops <k63.2412550051@ftu.edu.vn>
2026-09-03 22:53:58 +0700

    Change base path to '/Expense-Tracker/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/expensetracker/',
+    base: '/Expense-Tracker/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
